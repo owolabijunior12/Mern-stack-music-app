e694c27f80b838d59f53f16e93169df5ec3b7e04
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-03-09 14:41:57 +0100

    first commit

---
+++ server/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app=express();
+require("dotenv").config()
+const cors = require("cors");
+const {default : mongoose} = require ("mongoose")
+
+const crypto = require('crypto');
+const secret = crypto.randomBytes(32).toString('hex');
+console.log(secret);
+app.use(cors({origin: true}))
+
+
+
+app.get("/",(req, res)=>{   
+        return res.json("hello it your boy Iboytech......", refreshToken)           
+})
+
+//user auth routes
+const userRoutes =require("./routes/auth");
+const router = require("./routes/auth");
+const { refreshToken } = require("firebase-admin/app");
+app.use("/api/user/",userRoutes)
+
+
+
+mongoose.connect(process.env.DB_STRINGS,{useNewurlParser : true});
+
+//  const doc = { name: 'John Doe', age: 30, email: 'johndoe@example.com' };
+
+
+mongoose.connection
+.once("open", ()=>console.log("connected to MongoDB DataBase is connected........"))
+.on("error",(err)=>{console.log(`Not  connecting to mongoDB ${err}`)})
+app.listen(4000,()=>{
+    console.log("Server started and listen to port 4000");
+})
+
+ module.exports = router
+
---
+++ server/routes/auth.js
@@ -0,0 +1,72 @@
+const router = require("express").Router();
+const user = require("../modules/user");
+const admin = require("../configuration/firebase.configuration");
+// const userSchema = require("../modules/user");
+router.get("/login", async (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(500).json({ message: "Authorization header not found" });
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+  try {
+    const decodeValue = await admin.auth().verifyIdToken(token);
+    if (!decodeValue) {
+      return res.status(505).json({ message: "un authorized" });
+    } else { 
+      // return res.status(200).json({ decodeValue });
+      // checking user existance
+      const userExists = await user.findOne({"user_id":decodeValue.user_id});
+      if(!userExists){
+        // return res.send("user not found"); 
+        newUserData(decodeValue ,req , res)
+      }else{
+        updateNewData( decodeValue , req,res)
+      }
+    }
+  } catch (error) {
+    return res.status(505).json({ message: "Invalid token" });
+  }
+}); 
+
+const newUserData = async (decodeValue, req, res) => {
+  const newUser = new user({
+    name: decodeValue.name,
+    email: decodeValue.email,
+    imageURL: decodeValue.picture,
+    user_id: decodeValue.user_id,
+    email_verfied: decodeValue.email_verified,
+    role: "member",
+    auth_time: decodeValue.auth_time,
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(200).send({ user: savedUser });
+  } catch (err) {
+    res.status(400).send({ success: false, msg: err });
+  }
+};
+
+const updateNewData = async(decodeValue, req ,res)=>{
+  const filter = {user_id: decodeValue.user_id};
+  const option ={
+    upsert : true,
+    new: true,
+  };
+
+    try {
+        const result = await user.findOneAndUpdate(
+          filter,
+          {auth_time : decodeValue.auth_time },
+          option
+        )
+        res.status(200).send({user:result });
+    } catch (error) {
+      res.status(400).send({success :false, msg :error})
+    }
+}
+
+
+module.exports = router;
+
+
+
