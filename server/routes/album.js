378a131d445609e0b22a7092c75113c228a1e12d
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-03-12 07:34:09 +0100

    done creating route functionality

--- server/routes/album.js
+++ server/routes/album.js
@@ -1,7 +1,70 @@
-const router  =require("express").Router();
+const express = require("express");
+const router = express.Router();
+const Album = require("../modules/album");
 
-router.get("/getAll", async (req,res) =>{
-    return res.json("hello it your boy Iboytech he got you album......")
-})
+router.post("/save", async (req, res, next) => {
+  try {
+    const createdAlbum = await Album.create({
+            name:req.body.name,
+            imageURL: req.body.imageURL
+        });
+    res.status(200).send({ success: true, album: createdAlbum });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
-module.exports = router
+router.get("/getOne/:id", async (req, res, next) => {
+  try {
+    const album = await Album.findById(req.params.id);
+    if (!album) {
+      return res.status(404).send({ error: "Album not found" });
+    }
+    res.status(200).json(album);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get("/getAll", async (req, res, next) => {
+  try {
+    const albums = await Album.find().sort({ createdAt: 1 });
+    res.status(200).json({ success: true, albums });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.put("/update/:id", async (req, res, next) => {
+  try {
+    const { name, imageURL } = req.body;
+    const album = await Album.findByIdAndUpdate(req.params.id, { name, imageURL });
+    res.status(200).send({ success: true, album });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.delete("/delete/:id", async (req, res, next) => {
+  try {
+    const album = await Album.deleteOne({ _id: req.params.id });
+    if (!album.deletedCount) {
+      return res.status(404).send({ success: false, error: "Album not found" });
+    }
+    res.status(200).send({ success: true, message: "Album deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ error: "Internal Server Error" });
+});
+
+module.exports = router;
--- server/routes/artist.js
+++ server/routes/artist.js
@@ -41,8 +41,33 @@ router.get("/getAll", async (req, res) => {
   }
 });
 
-router.delete("/delete/:id",async (req,res) =>{
-    
-})
+router.put("/update/:id", async (req, res) => {
+  try {
+    const artist = await Artist.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+      imageURL: req.body.imageURL,
+      twitter: req.body.twitter,
+      instagram: req.body.instagram,
+      facebook: req.body.facebook,
+    });
+    res.status(200).send({ success: true, artist: artist });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Failed to update artist" });
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const artist = await Artist.deleteOne({ _id: req.params.id });
+    if (!artist.deletedCount) {
+      return res.status(404).send({ success: false, error: "Artist not found" });
+    }
+    res.status(200).send({ success: true, message: "Artist deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ success: false, error: "Failed to delete artist" });
+  }
+});
 
 module.exports = router;
