08f32d0e9d9b96e43fd1f7c4043b2c5b65cb1a40
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-03-22 14:28:49 +0100

    debugging

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,30 +1,34 @@
-const router = require("express").Router();
-const user = require("../modules/user");
-const admin = require("../configuration/firebase.configuration");
+const router = require('express').Router();
+const user = require('../modules/user');
+const admin = require('../configuration/firebase.configuration');
 
-router.get("/login", async (req, res) => {
-  if (!req.headers.authorization) {
-    return res.status(401).json({ message: "Authorization header not found" });
-  }
 
-  const token = req.headers.authorization.split(" ")[1];
+router.get('/login', async (req, res) => {
   try {
+    //! Get the authorization header and decode the value
+
+
+    const { authorization } = req.headers;
+    const [, token] = authorization.split(' ');
     const decodeValue = await admin.auth().verifyIdToken(token);
+
     if (!decodeValue) {
-      return res.status(401).json({ message: "Unauthorized" });
-    } else { 
-      const userExists = await user.findOne({"user_id":decodeValue.user_id});
-      if(!userExists){
-        newUserData(decodeValue ,req , res);
-      }else{
-        updateNewData(decodeValue, req, res);
-      }
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const userExists = await user.findOne({ user_id: decodeValue.user_id });
+    if (!userExists) {
+      newUserData(decodeValue, req, res);
+    } else {
+      updateNewData(decodeValue, req, res);
     }
   } catch (error) {
     console.error(error);
-    return res.status(401).json({ message: "Invalid token" });
+    return res.status(401).json({ message: 'Invalid token' });
   }
-}); 
+});
+//! Function to create a new user if the user does not exist
+
 
 const newUserData = async (decodeValue, req, res) => {
   const newUser = new user({
@@ -33,7 +37,7 @@ const newUserData = async (decodeValue, req, res) => {
     imageURL: decodeValue.picture,
     user_id: decodeValue.user_id,
     email_verfied: decodeValue.email_verified,
-    role: "member",
+    role: 'member',
     auth_time: decodeValue.auth_time,
   });
   try {
@@ -44,43 +48,67 @@ const newUserData = async (decodeValue, req, res) => {
   }
 };
 
-const updateNewData = async(decodeValue, req ,res)=>{
-  const filter = {user_id: decodeValue.user_id};
-  const option ={
-    upsert : true,
+//! Function to update user data if the user already exists
+
+
+const updateNewData = async (decodeValue, req, res) => {
+  const filter = { user_id: decodeValue.user_id };
+  const options = {
+    upsert: true,
     new: true,
   };
-
-    try {
-        const result = await user.findOneAndUpdate(
-          filter,
-          {auth_time : decodeValue.auth_time },
-          option
-        )
-        return res.status(200).send({user:result });
-    } catch (error) {
-      console.error(error);
-      return res.status(400).send({success :false, msg :error})
-    }
-}
-
-router.get("/getUser", async (req, res) => {
   try {
-    const option = {
-      sort: {
-        createdAt: 1,
-      },
-    };
-    const cursor = await user.find(option);
-    if (cursor.length > 0) {
-      return res.status(200).send({ success: true, data: cursor });
-    } else {
-      return res.status(404).send({ success: false, msg: "Not found" });
-    }
+    const result = await user.findOneAndUpdate(filter, { auth_time: decodeValue.auth_time }, options);
+    return res.status(200).send({ user: result });
   } catch (error) {
     console.error(error);
-    return res.status(500).send({ success: false, msg: "Internal server error" });
+    return res.status(400).send({ success: false, msg: error });
+  }
+};
+
+//! Route to authenticate user
+
+
+// router.get('/login', async (req, res) => {
+//   try {
+//     //! Get the authorization header and decode the value
+
+
+//     const { authorization } = req.headers;
+//     const [, token] = authorization.split(' ');
+//     const decodeValue = await admin.auth().verifyIdToken(token);
+
+//     if (!decodeValue) {
+//       return res.status(401).json({ message: 'Unauthorized' });
+//     }
+
+//     const userExists = await user.findOne({ user_id: decodeValue.user_id });
+//     if (!userExists) {
+//       newUserData(decodeValue, req, res);
+//     } else {
+//       updateNewData(decodeValue, req, res);
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(401).json({ message: 'Invalid token' });
+//   }
+// });
+
+//! Route to get all users
+
+
+router.get('/getUser', async (req, res) => {
+  const options = {
+    sort: { createdAt: 1 },
+    user:user.user
+  };
+
+  const cursor = await user.find(options);
+  if (cursor) {
+    res.status(200).send({ success: true, user:cursor });
+  } else {
+    res.status(200).send({ success: true, msg: 'No Data Found' });
   }
 });
-module.exports = router;
 
+module.exports = router;
